f9541965dd14fc1182e1629a1a0301e1a533f5a5
Ananyamn-21 <147376107+Ananyamn-21@users.noreply.github.com>
2024-05-30 15:53:03 +0530

    Initial commit

---
+++ Conditional.py
@@ -0,0 +1,35 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format
+
+# A single line containing a positive integer, .
+
+# Constraints
+
+# Output Format
+
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if(n%2!=0):
+       print("Weird")
+elif(n%2==0 and n>=2 and n<=5):
+       print("Not Weird")
+elif(n%2==0 and n>=6 and n<=20):
+       print("Weird")
+elif(n%2==0 and n>20):
+       print("Not Weird")
